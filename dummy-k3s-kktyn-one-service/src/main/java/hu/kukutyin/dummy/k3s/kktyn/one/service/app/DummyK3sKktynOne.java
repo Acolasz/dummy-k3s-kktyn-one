1c5254aff84203ed19027697bb2dce797e1d42de
Olivér <oliver.ocsko@icellmobilsoft.hu>
2024-01-07 10:55:16 +0100

    added empty springboot

---
+++ dummy-k3s-kktyn-one-service/src/main/java/hu/kukutyin/dummy/k3s/kktyn/one/service/app/DummyK3sKktynOne.java
@@ -0,0 +1,13 @@
+package hu.kukutyin.dummy.k3s.kktyn.one.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyK3sKktynOne {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyK3sKktynOne.class, args);
+    }
+
+}
---
+++ dummy-k3s-kktyn-one-service/src/test/java/hu/kukutyin/dummy/k3s/kktyn/one/service/app/DummyK3sKktynOneTests.java
@@ -0,0 +1,14 @@
+package hu.kukutyin.dummy.k3s.kktyn.one.service.app;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyK3sKktynOneTests {
+
+    @Test
+    void apiContextLoads() {
+        // empty
+    }
+}
