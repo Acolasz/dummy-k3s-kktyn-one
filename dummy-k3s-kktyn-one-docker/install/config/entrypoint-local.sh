645390e9c678a6541d654b7207906c0e4e7e60c8
Olivér <oliver.ocsko@icellmobilsoft.hu>
2024-01-17 21:01:27 +0100

    setup env SERVICE_PORT_SERVICE and SERVICE_PORT_DEBUG

--- dummy-k3s-kktyn-one-docker/install/config/entrypoint-local.sh
+++ dummy-k3s-kktyn-one-docker/install/config/entrypoint-local.sh
@@ -2,10 +2,10 @@
 
 export JAVA_OPTS="-Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8 -XX:+UseContainerSupport -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses -Djava.security.egd=file:/dev/./urandom"
 
-if [ "$DEBUG_PORT" ]; then
-  export JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:$DEBUG_PORT"
+if [ "${SERVICE_PORT_DEBUG}" ]; then
+  export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:${SERVICE_PORT_DEBUG}"
 fi
 
 set -e
 
-exec java -Dserver.port=$PORT $JAVA_OPTS -jar ${SPRING_APP_LOCATION}/service.jar --spring.config.location=file:${SPRING_CONFIG_LOCATION}/
+exec java -Dserver.port=${SERVICE_PORT_SERVICE} ${JAVA_OPTS} -jar ${SPRING_APP_LOCATION}/service.jar --spring.config.location=file:${SPRING_CONFIG_LOCATION}/
